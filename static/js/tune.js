7ded73bc384b3682e13f911a9fe3ef0a40f4f34d
HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>
2024-09-15 14:23:10 +0200

    add library filter

--- static/js/tune.js
+++ static/js/tune.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let licenceFilter = new AnyOfFilter("filter-licence", "all-tunes", filters);
     let languageFilter = new AnyOfFilter("filter-language", "all-tunes", filters);
     let tagFilter = new AllOfFilter("filter-tags", "all-tunes", filters);
+    let libraryFilter = new AnyOfFilter("filter-library", "all-tunes", filters);
     filters.push(
         maturityFilter,
         licenceFilter,
         languageFilter,
-        tagFilter
+        tagFilter,
+        libraryFilter
     );
     refreshCardsView("all-tunes", filters);
     for(const filter of filters) {
