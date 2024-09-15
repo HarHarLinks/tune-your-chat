afb79830a276a06d05ac12ba5258a6eb82285f2f
HarHarLinks <2803622+HarHarLinks@users.noreply.github.com>
2024-09-15 14:22:22 +0200

    enable maturity filter

--- static/js/tune.js
+++ static/js/tune.js
@@ -3,12 +3,12 @@ import { AllOfFilter, AnyOfFilter, refreshCardsView } from "./projects.js";
 document.addEventListener('DOMContentLoaded', (event) => {
 
     var filters = [];
-//    let maturityFilter = new AnyOfFilter("filter-maturity", "all-tunes", filters);
+    let maturityFilter = new AnyOfFilter("filter-maturity", "all-tunes", filters);
     let licenceFilter = new AnyOfFilter("filter-licence", "all-tunes", filters);
     let languageFilter = new AnyOfFilter("filter-language", "all-tunes", filters);
     let tagFilter = new AllOfFilter("filter-tags", "all-tunes", filters);
     filters.push(
-//        maturityFilter,
+        maturityFilter,
         licenceFilter,
         languageFilter,
         tagFilter
